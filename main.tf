e2ad7c7768e5dea6ba73acb44d0eb4884844730c
Gerard Nguyen <gerard@hashicorp.com>
2021-01-22 14:08:08 +1100

    Change AWS region for S3 module

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "s3-webapp" {
   source  = "app.terraform.io/gerard-nguyen-org/s3-webapp/aws"
   name    = var.name
-  region  = var.region
+  region  = "us-west-1"
   prefix  = var.prefix
   version = "1.0.0"
 }
